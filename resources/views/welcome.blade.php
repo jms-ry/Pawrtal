bdf3d96546bcf530e1f4e57b2938d7595502ad05
jms-ry <106312994+jms-ry@users.noreply.github.com>
2025-06-14 21:52:12 +0800

    Bootswatch and Bootstrap-icons setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="bg-success m-5 p-5">
+  <div class="bg-primary m-5 p-5">
     <h1 class="text-center">Welcome to Pawrtal</h1>
+    <button type="button" class="btn btn-danger">
+      <i class="bi bi-exclamation"></i> Test
+    </button>
     <span>#Landing Page</span>
   </div>
 @endsection
