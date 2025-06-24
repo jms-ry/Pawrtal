845317cdd354904fbc2d35c02b67d55a2869ef53
jms-ry <106312994+jms-ry@users.noreply.github.com>
2025-06-25 00:52:20 +0800

    Connect rescues route to AnimalController index
    Updated the /rescues route to use AnimalController@index, passing all Animal records to the rescues.index view. Removed the previous closure-based route for /rescues.

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -12,7 +12,9 @@ class AnimalController extends Controller
      */
     public function index()
     {
-        //
+      return view('rescues.index',[
+        'rescues'=> Animal::all(),
+    ]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
+use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[WelcomeController::class, 'index'])->name('welcome');
+Route::get('/rescues',[AnimalController::class,'index'])->name('rescues.index');
 
-Route::get('/rescues', function () {
-    return view('rescues.index');
-})->name('rescues');
 
 Route::get('/reports', function () {
     return view('reports.index');
