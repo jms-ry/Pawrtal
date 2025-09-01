01607766ac14a97c163ccbfef0459269f6d61daf
jms-ry <jamesroyxd30@gmail.com>
2025-09-01 13:29:56 +0800

    Vue and Inertia Setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,15 @@ import 'aos/dist/aos.css';
 // Initialize AOS
 AOS.init();
 import './libs';
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}.vue`),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
