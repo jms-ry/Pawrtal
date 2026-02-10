4aa98149e6e74a3c08b85be99c112cb1794de559
jms-ry <jamesroyxd30@gmail.com>
2026-02-10 19:43:20 +0800

    test: temporarily set cleanup to 5 minutes for testing

--- app/Console/Commands/CleanupExpiredDonations.php
+++ app/Console/Commands/CleanupExpiredDonations.php
@@ -16,7 +16,7 @@ class CleanupExpiredDonations extends Command
   {
     $expiredDonations = Donation::where('donation_type', 'monetary')
       ->where('payment_status', 'pending')
-      ->where('donation_date', '<', Carbon::now()->subHours(1))
+      ->where('donation_date', '<', Carbon::now()->subMinutes(5))
     ->get();
 
     $count = $expiredDonations->count();
