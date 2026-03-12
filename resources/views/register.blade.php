a4756ab7d35e2f9d786fc69eb1385bd8d843b793
jms-ry <jamesroyxd30@gmail.com>
2026-03-12 21:44:21 +0800

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -71,5 +71,10 @@
         </form>
       </div>
     </div>
+    <form action="{{ route('test-419') }}" method="POST">
+    @csrf
+    <input type="text" name="email" value="test@example.com">
+    <button type="submit">Test 419 Error</button>
+</form>
   </div>
 @endsection
