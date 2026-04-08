5ef5e2c7297110f904a51ce199044ca385ff73cc
jms-ry <jamesroyxd30@gmail.com>
2026-04-08 08:31:32 +0800

    Revert "feat: fix logo"
    This reverts commit a2ab8e12d60666aaac89dd547b30eb12e4bc0c63.

--- resources/js/Components/Modals/Donate/MonetaryDonationModal.vue
+++ resources/js/Components/Modals/Donate/MonetaryDonationModal.vue
@@ -61,7 +61,7 @@
               :disabled="loading"
             >
               <span class="d-flex align-items-center">
-                <img src="/images/donate/logo.png" alt="GCash" class="me-2" style="height: 22px">
+                <img src="/images/donate/image.png" alt="GCash" class="me-2" style="height: 22px">
               </span>
               <i class="bi bi-check-circle-fill" v-if="method === 'gcash'"></i>
             </button>
