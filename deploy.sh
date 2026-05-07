5425bd82e1d8ef231ae3a98381983ab54b974ddc
jms-ry <jamesroyxd30@gmail.com>
2026-05-07 08:52:33 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,17 @@ echo "================================"
 echo "CUSTOM DEPLOY SCRIPT RUNNING"
 echo "================================"
 
+#Add :fresh in the migrate to reset database. Remove it after first deployment to avoid data loss. 
 echo ""
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --force
 
 #TODO: Remove the seeding after the first deployment -- comment out "php artisan db:seed --force" line below. 
 
-# echo ""
-# echo "SEEDING DATABASE NOW..."
-# php artisan db:seed --force
-# echo "SEEDING COMPLETED!"
+echo ""
+echo "SEEDING DATABASE NOW..."
+php artisan db:seed --force
+echo "SEEDING COMPLETED!"
 
 echo ""
 echo "Linking storage..."
